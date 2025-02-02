9046835d0a7371a7b33bae84034aa56c676338eb
drjpeibol <pablo.serranomolinero@gmail.com>
2025-02-02 23:50:50 +0100

    Initial Commit

---
+++ FritApp/FritAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FritAppApp.swift
+//  FritApp
+//
+//  Created by Pablo Serrano Molinero on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FritAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FritAppTests/FritAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FritAppTests.swift
+//  FritAppTests
+//
+//  Created by Pablo Serrano Molinero on 2/2/25.
+//
+
+import Testing
+@testable import FritApp
+
+struct FritAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
